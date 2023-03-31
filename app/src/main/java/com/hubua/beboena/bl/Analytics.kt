bbb9835645851b428c62ed01e0a7372585e7d61a
George Khubua <george.khubua@live.com>
2023-03-31 12:41:16 +0300

    Add analytics

--- app/src/main/java/com/hubua/beboena/bl/Analytics.kt
+++ app/src/main/java/com/hubua/beboena/bl/Analytics.kt
@@ -9,6 +9,8 @@ object Analytics {
 
     private val firebaseAnalytics = Firebase.analytics
 
+    private val currentLetter get() = GeorgianAlphabet.Cursor.currentLetter
+
     fun logScreenView(name: String) {
         // https://firebase.google.com/docs/reference/android/com/google/firebase/analytics/FirebaseAnalytics.Event#public-static-final-string-screen_view
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
@@ -20,7 +22,7 @@ object Analytics {
         // https://firebase.google.com/docs/reference/android/com/google/firebase/analytics/FirebaseAnalytics.Event#public-static-final-string-level_end
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LEVEL_END) {
             param(FirebaseAnalytics.Param.LEVEL_NAME, levelName)
-            param(FirebaseAnalytics.Param.LEVEL, "Letter ${GeorgianAlphabet.Cursor.currentLetter.learnOrder} - ${GeorgianAlphabet.Cursor.currentLetter.letterModernSpelling}")
+            param(FirebaseAnalytics.Param.LEVEL, "Letter ${currentLetter.learnOrder.toString().padStart(3, '0')} - ${currentLetter.letterModernSpelling}")
             param(FirebaseAnalytics.Param.SUCCESS, success)
         }
     }
@@ -28,7 +30,7 @@ object Analytics {
     fun logPostScore(score: String, percent: Long) {
         // https://firebase.google.com/docs/reference/android/com/google/firebase/analytics/FirebaseAnalytics.Event#public-static-final-string-post_score
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.POST_SCORE) {
-            param(FirebaseAnalytics.Param.LEVEL, "Letter ${GeorgianAlphabet.Cursor.currentLetter.learnOrder} - ${GeorgianAlphabet.Cursor.currentLetter.letterModernSpelling}")
+            param(FirebaseAnalytics.Param.LEVEL, "Letter ${currentLetter.learnOrder.toString().padStart(3, '0')} - ${currentLetter.letterModernSpelling}")
             param(FirebaseAnalytics.Param.SCORE, score)
             param("score_percent", percent)
         }
